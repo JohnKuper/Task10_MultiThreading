ac1da0dcf7554bfe86fb37894a67bc5aa51a0d3b
JohnKuper <bulletstorm360@gmail.com>
2014-09-30 09:29:36 +0400

    add watcher

--- src/main/java/com/johnkuper/main/Main.java
+++ src/main/java/com/johnkuper/main/Main.java
@@ -1,14 +1,14 @@
 package com.johnkuper.main;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.johnkuper.dbconnect.ConnectionHelper;
+import com.johnkuper.watcher.WatchDir;
 
 public class Main {
 
@@ -17,24 +17,25 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Connection conn = conHelper.getConnection();
+		/*
+		 * Connection conn = conHelper.getConnection(); try { Statement stmt =
+		 * conn.createStatement(); ResultSet rs;
+		 * 
+		 * rs = stmt .executeQuery(
+		 * "SELECT bank_account,TIN,company_name,company_address From legal_payers"
+		 * ); while (rs.next()) { logger.debug(rs.getString(1));
+		 * logger.debug(rs.getString(2)); logger.debug(rs.getString(3));
+		 * logger.debug(rs.getString(4)); } conn.close();
+		 * 
+		 * } catch (SQLException ex) { logger.error("SQL error", ex); } finally
+		 * { }
+		 */
+
+		Path dir = Paths.get("src/main/resources");
 		try {
-			Statement stmt = conn.createStatement();
-			ResultSet rs;
-
-			rs = stmt
-					.executeQuery("SELECT bank_account,TIN,company_name,company_address From legal_payers");
-			while (rs.next()) {
-				logger.debug(rs.getString(1));
-				logger.debug(rs.getString(2));
-				logger.debug(rs.getString(3));
-				logger.debug(rs.getString(4));
-			}
-			conn.close();
-
-		} catch (SQLException ex) {
-			logger.error("SQL error", ex);
-		} finally {
+			new WatchDir(dir).processEvents();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
---
+++ src/main/java/com/johnkuper/watcher/WatchDir.java
@@ -0,0 +1,106 @@
+package com.johnkuper.watcher;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchEvent.Kind;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WatchDir {
+
+	private final WatchService watcher;
+	private final Map<WatchKey, Path> keys;
+	private boolean trace = false;
+
+	@SuppressWarnings("unchecked")
+	static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+		return (WatchEvent<T>) event;
+	}
+
+	/**
+	 * Register the given directory with the WatchService
+	 */
+	private void register(Path dir) throws IOException {
+		WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE);
+		if (trace) {
+			Path prev = keys.get(key);
+			if (prev == null) {
+				System.out.format("register: %s\n", dir);
+			} else {
+				if (!dir.equals(prev)) {
+					System.out.format("update: %s -> %s\n", prev, dir);
+				}
+			}
+		}
+		keys.put(key, dir);
+	}
+
+	/**
+	 * Creates a WatchService and registers the given directory
+	 */
+	public WatchDir(Path dir) throws IOException {
+		this.watcher = FileSystems.getDefault().newWatchService();
+		this.keys = new HashMap<WatchKey, Path>();
+		this.trace = true;
+		register(dir);
+	}
+
+	/**
+	 * Process all events for keys queued to the watcher
+	 */
+	public void processEvents() {
+		for (;;) {
+
+			// wait for key to be signalled
+			WatchKey key;
+			try {
+				key = watcher.take();
+			} catch (InterruptedException x) {
+				return;
+			}
+
+			Path dir = keys.get(key);
+			if (dir == null) {
+				System.err.println("WatchKey not recognized!!");
+				continue;
+			}
+
+			for (WatchEvent<?> event : key.pollEvents()) {
+				Kind<?> kind = event.kind();
+
+				if (kind == OVERFLOW) {
+					continue;
+				}
+
+				// Context for directory entry event is the file name of entry
+				WatchEvent<Path> ev = cast(event);
+				Path name = ev.context();
+				Path child = dir.resolve(name);
+
+				// print out event
+				System.out.format("%s: %s\n", event.kind().name(), child);
+
+			}
+
+			// reset key and remove from set if directory no longer accessible
+			boolean valid = key.reset();
+			if (!valid) {
+				keys.remove(key);
+
+				// all directories are inaccessible
+				if (keys.isEmpty()) {
+					break;
+				}
+			}
+		}
+	}
+
+}
